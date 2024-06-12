5d37d91e7d5c57b74ec33750eaef9ef542370ed4
unknown <true.for.fun@gmail.com>
2024-06-12 22:34:20 +0300

    use imperative in use

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import Modal from './Modal'
+
 
 function App() {
+  const modalRef = useRef()
+
+  const handleOpenModal = () => {
+    modalRef.current.openModal()
+  }
+  console.log('parent rendered')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Parent Component</p>
+      <Modal ref={modalRef} />
+      <button onClick={handleOpenModal}>Open</button>
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { forwardRef, useImperativeHandle, useState } from 'react'
+
+const Modal = (props, ref) => {
+  const [modalState, setModalState] = useState(false)
+  useImperativeHandle(ref, () => ({
+    openModal: () => setModalState(true)
+  }))
+  console.log('child rendered')
+  if (!modalState) return null
+  return (
+    <div className='modal'>
+      <p>This is my modal!</p>
+      <button onClick={() => setModalState(false)}>Close</button>
+
+    </div>
+  )
+}
+
+export default forwardRef(Modal)
